03f348d8df75ba7e1e266a66be0bfc160c8382ec
Manjit kumar <167091085+Manjit559@users.noreply.github.com>
2025-02-04 20:53:44 +0530

    Create Dynamic programming.java

---
+++ Dynamic programming.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    private static int stairs(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int numStairs = 3;
+        System.out.println(stairs(numStairs));
+    }
+}
